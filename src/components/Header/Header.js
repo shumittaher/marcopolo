5e4d2e3b1ec47a2c8567dc945a78d5e78faaad25
shumit <shumitt@gmail.com>
2021-10-29 02:29:26 +0600

    ok

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -23,7 +23,7 @@ const Header = () => {
 
                 <div className="flex items-center  space-x-4 px-5">
                     <span>Logged in as: {user.email}</span>
-                    <Link to="/admin"> <button className="p-2">Admin Page</button ></Link>
+                    <Link to="/admin"> <button className="p-2">Admin</button ></Link>
                     <button className="p-2" onClick={logOut}>LogOut</button>
                 </div>
 
