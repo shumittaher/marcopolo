58ca2e9af99e14833b9ca89b6860fa8525a7f09a
shumit <shumitt@gmail.com>
2021-10-31 17:38:11 +0600

    responsive

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -26,9 +26,9 @@ const Banner = () => {
                 <img className="object-cover w-screen h-screen" src={imageArray[bannerImage]} alt="" />
             </div>
 
-            <div className="absolute top-1/3 w-full text-center text-white z-10 ">
+            <div className="absolute top-1/3 right-1/3 md:left-auto left-5 text-white z-10 ">
                 <h6 className="md:text-4xl text-xl text-indigo-900	font-bold	">Find your special tour today</h6>
-                <h1 className="md:text-8xl text-4xl"> With Marco Polo Tours</h1>
+                <h1 className="md:text-8xl text-3xl"> With Marco Polo Tours</h1>
 
                 <Link to="/destinations"><button className="btn-big ">
                     Explore More
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,48 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import useAuth from './../../hooks/useAuth';
 import headerlogo from '../../images/designpics/headerlogo.png'
-import { HashLink  } from 'react-router-hash-link';
+import { HashLink } from 'react-router-hash-link';
 
 
 const Header = () => {
 
     const { user, logOut } = useAuth()
 
+    const [menuStyle, setMenuStyle] = useState("none")
+
+    const handleToggle = () => menuStyle === "none" ? setMenuStyle("flex") : setMenuStyle("none")
+
     return (
         <div className="text-white bg-gray-400 bg-opacity-50 absolute w-full z-10 flex h-24 items-center justify-between">
 
 
-                <img src={headerlogo} className="md:w-48 w-32 self-start " alt="" />
+            <img src={headerlogo} className="md:w-48 w-32 self-start " alt="" />
 
-                <div className="flex-grow md:flex justify-start space-x-4 ml-5 hidden">
-
-
-                    <Link to="/home"><button className="btn">Home</button></Link>
+            <div className="flex-grow md:flex justify-start space-x-4 ml-5 hidden">
 
 
-                    <Link to="/destinations"> <button className="btn">Destinations</button ></Link>
-
-                    <HashLink to="/home#about"> <button className="btn">About</button ></HashLink>
+                <Link to="/home"><button className="btn">Home</button></Link>
 
 
-                </div>
+                <Link to="/destinations"> <button className="btn">Destinations</button ></Link>
+
+                <HashLink to="/home#about"> <button className="btn">About</button ></HashLink>
 
 
-                <div className="md:flex items-center  hidden space-x-4 px-5">
-
-                    {user.email && <Link to={`/userPage/${user.email}`}> <button className="btn">Visit your page: {user.email} </button> </Link>}
-
-                    {!user.email && <Link to="/login"> <button className="btn">Login</button></Link>}
-
-                    <Link to="/admin"> <button className="btn">Admin</button ></Link>
-                    {user.email && <button className="btn" onClick={logOut}>LogOut</button>}
+            </div>
 
 
-                </div>
+            <div className="md:flex items-center  hidden space-x-4 px-5">
 
-                    <i className="fas fa-bars block text-5xl md:hidden mr-5"></i>
+                {user.email && <Link to={`/userPage/${user.email}`}> <button className="btn">Visit your page: {user.email} </button> </Link>}
 
+                {!user.email && <Link to="/login"> <button className="btn">Login</button></Link>}
+
+                <Link to="/admin"> <button className="btn">Admin</button ></Link>
+                {user.email && <button className="btn" onClick={logOut}>LogOut</button>}
+
+
+            </div>
+
+            <i onClick={handleToggle} className="fas fa-bars block text-5xl md:hidden mr-5 cursor-pointer"></i>
+
+            <div style={{ display: `${menuStyle}` }} className="absolute bg-yellow-50 flex-col top-24 right-0 text-right text-indigo-700 p-5 bg-opacity-80 ">
+
+                <Link to="/home"><button className="p-2 font-bold">Home</button></Link>
+
+
+                <Link to="/destinations"> <button className="font-bold p-2" n>Destinations</button ></Link>
+
+                <HashLink to="/home#about"> <button className=" font-bold p-2">About</button ></HashLink>
+
+                {user.email && <Link to={`/userPage/${user.email}`}> <button className=" font-bold p-2">Visit your page: {user.email} </button> </Link>}
+
+                {!user.email && <Link to="/login"> <button className=" font-bold p-2">Login</button></Link>}
+
+                <Link to="/admin"> 
+                <button className="font-bold p-2">Admin</button >
+                </Link>
+
+                {user.email && <button className=" font-bold p-2" onClick={logOut}>LogOut</button>}
+
+
+            </div>
 
         </div >
     )
