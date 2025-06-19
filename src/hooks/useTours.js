5faad3c7ccd9ac13d9ec956f54794048de2e62ab
Shumit Taher <ShumitT@gmail.com>
2025-06-19 17:45:26 +1200

    fixed env

--- src/hooks/useTours.js
+++ src/hooks/useTours.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 const apiUrl = process.env.REACT_APP_API_URL;
-
+// const apiUrl = 'https://marcopolo-server.onrender.com';
 
 const useTours = () => {
 
